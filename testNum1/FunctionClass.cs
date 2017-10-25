c3acb5b13f9e3f0bba7c754acb8d212c44971801
Tzahiani <tanidgar@gmail.com>
2017-10-25 17:20:58 +0300

    Upload Project

---
+++ testNum1/FunctionClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+
+namespace testNum1
+{
+    class FunctionClass
+    {
+        IWebDriver browser;
+
+        public void openChrome()
+        {
+            browser = new ChromeDriver();
+        }
+
+        public void maxiChrome()
+        {
+            browser.Manage().Window.Maximize();
+        }
+
+        public void closeChrome()
+        {
+            browser.Close();
+        }
+
+        public void goToURL(string url)
+        {
+            browser.Navigate().GoToUrl(url);
+        }
+
+        public void insertDataToInputByID(string att, string str)
+        {
+            browser.FindElement(By.Id(att)).SendKeys(str);
+        }
+
+        public void insertDataToInputByClass(string att, string str)
+        {
+            browser.FindElement(By.ClassName(att)).SendKeys(str);
+        }
+        public void clickOnElement(string att)
+        {
+            browser.FindElement(By.XPath("//input[@" + att + "]")).Click();
+        }
+        public void waitFunc(int time)
+        {
+            browser.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(time));
+        }
+    }
+}
---
+++ testNum1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Remoting.Messaging;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace testNum1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //IWebDriver browser;
+        FunctionClass ff = new FunctionClass();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ff.openChrome();
+            ff.maxiChrome();
+            ff.goToURL("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F");
+            ff.insertDataToInputByID("userid", "tanidgar@gmail.com");
+            ff.insertDataToInputByID("pass", "Aa15061986");
+            ff.clickOnElement("id='sgnBt'");
+            ff.waitFunc(10);
+            ff.goToURL("http://bulksell.ebay.com/ws/eBayISAPI.dll?SingleList&&DraftURL=http://www.ebay.com/sh/lst/drafts&sellingMode=AddItem&templateId=5202621013&returnUrl=http://bulksell.ebay.com/ws/eBayISAPI.dll?SingleList");
+            
+            var json = new WebClient().DownloadString("https://api.magicthegathering.io/v1/cards?name=Abrade");
+            ff.waitFunc(10);
+            JObject obj = JObject.Parse(json);
+            string name = obj["cards"][0]["name"].Value<string>();
+
+            ff.insertDataToInputByID("editpane_title", "***4X " + name + "***");
+            //ff.insertDataToInputByClass("find-product","Abrade");
+            //ff.closeChrome();
+        }
+    }
+}
